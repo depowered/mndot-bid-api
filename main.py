b5eff6f7a5bd5744cb13263680d12388ff85993c
depowered <dpwr16@gmail.com>
2022-10-05 22:52:35 +0000

    sample data loading

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from sqlite3 import SQLITE_ALTER_TABLE
+from pathlib import Path
 
 from fastapi import FastAPI
 
-from mndot_bid_api.db import database
+from mndot_bid_api.db import database, sample_data
 from mndot_bid_api.routers import bidders, bids, contracts
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///data/api.db"
-# SQLALCHEMY_DATABASE_URL = "sqlite:///data/sample.db"
 
 
 app = FastAPI()
@@ -14,7 +13,11 @@ app = FastAPI()
 
 @app.on_event("startup")
 def startup_event():
-    database.init_sqlite_db(url=SQLALCHEMY_DATABASE_URL)
+    if not Path("./data/api.db").exists():
+        database.init_sqlite_db(url=SQLALCHEMY_DATABASE_URL)
+        sample_data.load_sample_data()
+    else:
+        database.init_sqlite_db(url=SQLALCHEMY_DATABASE_URL)
 
 
 @app.get("/", include_in_schema=False)
@@ -22,6 +25,6 @@ def read_root():
     return {"server status": "Running"}
 
 
-app.include_router(contracts.router)
+# app.include_router(contracts.router)
 app.include_router(bidders.router)
-app.include_router(bids.router)
+# app.include_router(bids.router)
--- mndot_bid_api/db/sample_data.py
+++ mndot_bid_api/db/sample_data.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from mndot_bid_api.db import database
 from mndot_bid_api.db.models import Bid, Bidder, Contract
 
 bidders = [
@@ -8,56 +9,62 @@ bidders = [
     Bidder(id=3, name="Ames"),
 ]
 
-contracts = [
-    Contract(
-        id=1,
-        is_processed=True,
-        let_date=datetime.today(),
-        let_year=2022,
-        spec_year=2020,
-        sp_number="4911-15",
-        district="Baxter",
-        county="Benton",
-        engineers_total=2_000_000_00,
-        lowest_bidder_id=3,
-        lowest_bidder_total=2_500_000_00,
-    ),
-    Contract(id=2, is_processed=False),
-    Contract(id=3, is_processed=False),
-]
+# contracts = [
+#     Contract(
+#         id=1,
+#         is_processed=True,
+#         let_date=datetime.today(),
+#         let_year=2022,
+#         spec_year=2020,
+#         sp_number="4911-15",
+#         district="Baxter",
+#         county="Benton",
+#         engineers_total=2_000_000_00,
+#         lowest_bidder_id=3,
+#         lowest_bidder_total=2_500_000_00,
+#     ),
+#     Contract(id=2, is_processed=False),
+#     Contract(id=3, is_processed=False),
+# ]
 
-bids = [
-    Bid(
-        id=1,
-        item_number="2501.503/02931",
-        spec_year=2020,
-        quantity=300.0,
-        unit_price=6_00,
-        total_price=int(300.0 * 6_00),
-        contract_id=1,
-        bidder_id=3,
-        bidder_rank=1,
-    ),
-    Bid(
-        id=2,
-        item_number="2575.503/12345",
-        spec_year=2020,
-        quantity=10_000.0,
-        unit_price=1_25,
-        total_price=int(10_000.0 * 1_25),
-        contract_id=1,
-        bidder_id=3,
-        bidder_rank=1,
-    ),
-    Bid(
-        id=3,
-        item_number="2575.501/00010",
-        spec_year=2020,
-        quantity=1.0,
-        unit_price=30_000_00,
-        total_price=int(1.0 * 30_000_00),
-        contract_id=1,
-        bidder_id=3,
-        bidder_rank=1,
-    ),
-]
+# bids = [
+#     Bid(
+#         id=1,
+#         item_number="2501.503/02931",
+#         spec_year=2020,
+#         quantity=300.0,
+#         unit_price=6_00,
+#         total_price=int(300.0 * 6_00),
+#         contract_id=1,
+#         bidder_id=3,
+#         bidder_rank=1,
+#     ),
+#     Bid(
+#         id=2,
+#         item_number="2575.503/12345",
+#         spec_year=2020,
+#         quantity=10_000.0,
+#         unit_price=1_25,
+#         total_price=int(10_000.0 * 1_25),
+#         contract_id=1,
+#         bidder_id=3,
+#         bidder_rank=1,
+#     ),
+#     Bid(
+#         id=3,
+#         item_number="2575.501/00010",
+#         spec_year=2020,
+#         quantity=1.0,
+#         unit_price=30_000_00,
+#         total_price=int(1.0 * 30_000_00),
+#         contract_id=1,
+#         bidder_id=3,
+#         bidder_rank=1,
+#     ),
+# ]
+
+
+def load_sample_data() -> None:
+    with database.DBSession() as db:
+        db.add_all(bidders)
+        db.commit()
