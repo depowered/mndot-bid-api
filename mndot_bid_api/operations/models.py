fb82361b9811e8fe2a70aa26c9b9ac7952059dc0
depowered <dpwr16@gmail.com>
2022-10-05 21:42:11 +0000

    update request and response schemas to match database

--- mndot_bid_api/operations/models.py
+++ mndot_bid_api/operations/models.py
@@ -1,92 +1,95 @@
 from datetime import date
 from typing import Optional
+
 from pydantic import BaseModel
-
-
-class ContractResult(BaseModel):
-    id: int
-    is_processed: bool
-    let_date: Optional[date]
-    let_year: Optional[int]
-    spec_year: Optional[int]
-    sp_number: Optional[str]
-    district: Optional[str]
-    county: Optional[str]
-    engineers_total: Optional[int]
-    lowest_bidder_id: Optional[int]
-    lowest_bidder_total: Optional[int]
 
 
 class ContractCreateData(BaseModel):
     id: int
-    is_processed: bool = False
-    let_date: date
-    let_year: Optional[int]
-    spec_year: Optional[int]
+    letting_date: date
     sp_number: str
     district: str
     county: str
-    engineers_total: int
-    lowest_bidder_id: int
-    lowest_bidder_total: int
+    description: str
+    winning_bidder_id: int
+    spec_year: str
+
+
+class ContractResult(ContractCreateData):
+    ...
 
 
 class ContractUpdateData(BaseModel):
-    is_processed: Optional[bool]
-    let_date: Optional[date]
-    let_year: Optional[int]
-    spec_year: Optional[int]
+    id: int
+    letting_date: Optional[date]
     sp_number: Optional[str]
     district: Optional[str]
     county: Optional[str]
-    engineers_total: Optional[int]
-    lowest_bidder_id: Optional[int]
-    lowest_bidder_total: Optional[int]
-
-
-class BidderResult(BaseModel):
-    id: int
-    name: Optional[str]
+    description: Optional[str]
+    winning_bidder_id: Optional[int]
+    spec_year: Optional[str]
 
 
 class BidderCreateData(BaseModel):
     id: int
-    name: Optional[str]
-
-
-class BidderUpdateData(BaseModel):
     name: str
 
 
-class BidResult(BaseModel):
-    id: int
-    item_number: str
-    spec_year: int
-    quantity: float
-    unit_price: int
-    total_price: int
-    contract_id: int
-    bidder_id: int
-    bidder_rank: int
+class BidderResult(BidderCreateData):
+    ...
+
+
+class BidderUpdateData(BidderCreateData):
+    ...
 
 
 class BidCreateData(BaseModel):
-    item_number: str
-    spec_year: int
+    contract_id: int
+    item_composite_id: str
+    bidder_id: int
     quantity: float
     unit_price: int
-    total_price: int
-    contract_id: int
-    bidder_id: int
-    bidder_rank: int
+    bid_type: str
+
+
+class BidResult(BidCreateData):
+    id: int
 
 
 class BidUpdateData(BaseModel):
-    item_number: Optional[str]
-    spec_year: Optional[int]
+    id: int
+    contract_id: Optional[int]
+    item_composite_id: Optional[str]
+    bidder_id: Optional[int]
     quantity: Optional[float]
     unit_price: Optional[int]
-    total_price: Optional[int]
-    contract_id: Optional[int]
-    bidder_id: Optional[int]
-    bidder_rank: Optional[int]
+    bid_type: Optional[str]
+
+
+class ItemCreateData(BaseModel):
+    composite_id: str
+    spec_year: str
+    spec_code: str
+    unit_code: str
+    item_code: str
+    short_description: str
+    long_description: str
+    unit: str
+    unit_abreviation: str
+
+
+class ItemResult(ItemCreateData):
+    id: int
+
+
+class ItemUpdateData(BaseModel):
+    id: int
+    composite_id: Optional[str]
+    spec_year: Optional[str]
+    spec_code: Optional[str]
+    unit_code: Optional[str]
+    item_code: Optional[str]
+    short_description: Optional[str]
+    long_description: Optional[str]
+    unit: Optional[str]
+    unit_abreviation: Optional[str]
