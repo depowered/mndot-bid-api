8afb6d512dc24c02909ad11bfa171efb85f8ee4e
depowered <dpwr16@gmail.com>
2022-10-06 23:45:36 +0000

    setup prod db

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from fastapi import FastAPI
 from mndot_bid_api.db import database, sample_data
 from mndot_bid_api.routers import bidders, bids, contracts, items
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///data/api.db"
+DEVELOPMENT_DATABASE_URL = "sqlite:///data/dev-api.db"
+PRODUCTION_DATABASE_URL = "sqlite:///data/prod-api.db"
+
+DEVELOPMENT_MODE = False
 
 
 app = FastAPI()
@@ -13,11 +16,14 @@ app = FastAPI()
 
 @app.on_event("startup")
 def startup_event():
-    if not Path("./data/api.db").exists():
-        database.init_sqlite_db(url=SQLALCHEMY_DATABASE_URL)
-        sample_data.load_sample_data()
+    dev_db_exists = Path("./data/dev-api.db").exists()
+
+    if DEVELOPMENT_MODE:
+        database.init_sqlite_db(url=DEVELOPMENT_DATABASE_URL)
+        if not dev_db_exists:
+            sample_data.load_sample_data()
     else:
-        database.init_sqlite_db(url=SQLALCHEMY_DATABASE_URL)
+        database.init_sqlite_db(url=PRODUCTION_DATABASE_URL)
 
 
 @app.get("/", include_in_schema=False)
