488dd0c455084b70d573149cdf9ee3181c00ede1
depowered <dpwr16@gmail.com>
2022-10-29 21:08:00 +0000

    refactor sample data to update data model

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from fastapi import FastAPI
 
 from mndot_bid_api import routers
-from mndot_bid_api.db import database  # , sample_data
+from mndot_bid_api.db import database, sample_data
 
 DEVELOPMENT_DATABASE_URL = "sqlite:///data/dev-api.db"
 PRODUCTION_DATABASE_URL = "sqlite:///data/prod-api.db"
@@ -21,8 +21,8 @@ def startup_event():
 
     if DEVELOPMENT_MODE:
         database.init_sqlite_db(url=DEVELOPMENT_DATABASE_URL)
-        # if not dev_db_exists:
-        #     sample_data.load_sample_data()
+        if not dev_db_exists:
+            sample_data.load_sample_data()
     else:
         database.init_sqlite_db(url=PRODUCTION_DATABASE_URL)
 
