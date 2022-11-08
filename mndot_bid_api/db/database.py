bd7cfbefeb4c065671040be3fbe14c7142c0deac
depowered <dpwr16@gmail.com>
2022-11-08 15:39:36 +0000

    remove unused get_db_session and SessionContextManager

--- mndot_bid_api/db/database.py
+++ mndot_bid_api/db/database.py
@@ -1,10 +1,7 @@
-from collections.abc import Generator
-
 from mndot_bid_api.db import models
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
-# engine: Engine | None = None
 DBSession = sessionmaker()
 
 
@@ -12,22 +9,3 @@ def init_sqlite_db(url: str) -> None:
     engine = create_engine(url, connect_args={"check_same_thread": False})
     DBSession.configure(bind=engine, autocommit=False, autoflush=False)
     models.Base.metadata.create_all(bind=engine)
-
-
-def get_db_session() -> Generator[Session, None, None]:
-    db: Session = DBSession()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-class SessionContextManager:
-    def __init__(self):
-        self.db = DBSession()
-
-    def __enter__(self):
-        return self.db
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.db.close()
