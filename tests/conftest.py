cd8b569fb434247b33decd5d114af17f4677b5f5
depowered <dpwr16@gmail.com>
2022-11-16 22:31:38 +0000

    setup FastAPI TestClient fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,17 +3,22 @@ Adapted from:
     https://stackoverflow.com/questions/58660378/how-use-pytest-to-unit-test-sqlalchemy-orm-classes
     https://gist.github.com/kissgyorgy/e2365f25a213de44b9a2
 """
+import fastapi
 import pytest
+from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
-from mndot_bid_api.db import models
+from mndot_bid_api import db, routers
+from mndot_bid_api.db import interface, models
 from tests.data import sample_db_records
 
 
 @pytest.fixture(scope="session")
 def engine():
-    return create_engine("sqlite:///tests/data/test-api.db")
+    return create_engine(
+        "sqlite:///tests/data/test-api.db", connect_args={"check_same_thread": False}
+    )
 
 
 @pytest.fixture(scope="session")
@@ -56,3 +61,75 @@ def configured_sessionmaker(engine, tables):
     transaction.rollback()
     # put back the connection to the connection pool
     connection.close()
+
+
+@pytest.fixture(scope="function")
+def get_bidder_interface_override(configured_sessionmaker: sessionmaker):
+    return lambda: interface.DBModelInterface(models.Bidder, configured_sessionmaker)
+
+
+@pytest.fixture(scope="function")
+def get_contract_interface_override(configured_sessionmaker: sessionmaker):
+    return lambda: interface.DBModelInterface(models.Contract, configured_sessionmaker)
+
+
+@pytest.fixture(scope="function")
+def get_bid_interface_override(configured_sessionmaker: sessionmaker):
+    return lambda: interface.DBModelInterface(models.Bid, configured_sessionmaker)
+
+
+@pytest.fixture(scope="function")
+def get_invalid_bid_interface_override(configured_sessionmaker: sessionmaker):
+    return lambda: interface.DBModelInterface(
+        models.InvalidBid, configured_sessionmaker
+    )
+
+
+@pytest.fixture(scope="function")
+def get_item_interface_override(configured_sessionmaker: sessionmaker):
+    return lambda: interface.DBModelInterface(models.Item, configured_sessionmaker)
+
+
+@pytest.fixture(scope="function")  # Or function?
+def test_client(
+    get_bidder_interface_override,
+    get_contract_interface_override,
+    get_bid_interface_override,
+    get_invalid_bid_interface_override,
+    get_item_interface_override,
+) -> TestClient:
+    # Initilize test_app instance
+    test_app = fastapi.FastAPI()
+
+    # Override get_<model>_interface functions
+    test_app.dependency_overrides[
+        db.get_bidder_interface
+    ] = get_bidder_interface_override
+
+    test_app.dependency_overrides[
+        db.get_contract_interface
+    ] = get_contract_interface_override
+
+    test_app.dependency_overrides[db.get_bid_interface] = get_bid_interface_override
+
+    test_app.dependency_overrides[
+        db.get_invalid_bid_interface
+    ] = get_invalid_bid_interface_override
+
+    test_app.dependency_overrides[db.get_item_interface] = get_item_interface_override
+
+    # Duplicate root path from main
+    @test_app.get("/", include_in_schema=False)
+    def read_root():
+        return {"server status": "Running"}
+
+    # Add all routes
+    test_app.include_router(routers.bidders_router)
+    test_app.include_router(routers.contracts_router)
+    test_app.include_router(routers.bids_router)
+    test_app.include_router(routers.invalid_bids_router)
+    test_app.include_router(routers.items_router)
+
+    # Itialize and return the test client
+    test_client = TestClient(test_app)
+    return test_client
--- tests/test_conftest_fixtures.py
+++ tests/test_conftest_fixtures.py
@@ -1,3 +1,4 @@
+from fastapi.testclient import TestClient
 from sqlalchemy.orm import sessionmaker
 
 from mndot_bid_api.db import models
@@ -19,3 +20,34 @@ def test_bidder_record_count_after_rollback(configured_sessionmaker: sessionmake
     with configured_sessionmaker() as dbsession:
         starting_record_count = len(dbsession.query(models.Bidder).all())
         assert starting_record_count == 6
+
+
+def test_get_bidder_interface_override(get_bidder_interface_override):
+    interface = get_bidder_interface_override()
+    assert isinstance(interface.model(), models.Bidder)
+
+
+def test_get_contract_interface_override(get_contract_interface_override):
+    interface = get_contract_interface_override()
+    assert isinstance(interface.model(), models.Contract)
+
+
+def test_get_bid_interface_override(get_bid_interface_override):
+    interface = get_bid_interface_override()
+    assert isinstance(interface.model(), models.Bid)
+
+
+def test_get_invalid_bid_interface_override(get_invalid_bid_interface_override):
+    interface = get_invalid_bid_interface_override()
+    assert isinstance(interface.model(), models.InvalidBid)
+
+
+def test_get_item_interface_override(get_item_interface_override):
+    interface = get_item_interface_override()
+    assert isinstance(interface.model(), models.Item)
+
+
+def test_test_app_running(test_client: TestClient):
+    response = test_client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"server status": "Running"}
