ee91d48ef7b8e56893b35aeaa3a1a102b923a322
depowered <dpwr16@gmail.com>
2022-11-20 02:22:41 +0000

    create generic raise HTTPException functions

--- mndot_bid_api/exceptions.py
+++ mndot_bid_api/exceptions.py
@@ -1,3 +1,6 @@
+from fastapi import HTTPException, status
+
+
 class RecordNotFoundError(Exception):
     pass
 
@@ -8,3 +11,24 @@ class RecordAlreadyExistsError(Exception):
 
 class InvalidBidError(Exception):
     pass
+
+
+def raise_http_404(model_name: str, id: int, exc: Exception) -> None:
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"{model_name} at ID {id} not found",
+    ) from exc
+
+
+def raise_http_404_query(model_name: str, exc: Exception) -> None:
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"No {model_name} records found matching the provided query parameters",
+    ) from exc
+
+
+def raise_http_303(model_name: str, id: int, exc: Exception) -> None:
+    raise HTTPException(
+        status_code=status.HTTP_303_SEE_OTHER,
+        detail=f"{model_name} already exists at ID {id}",
+    ) from exc
