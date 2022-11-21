f4479560199c2d360ea39d5f04c55f8e3d4482c2
depowered <dpwr16@gmail.com>
2022-11-21 22:31:54 +0000

    extend read_item_list_csv test to verify invalid df raises error

--- tests/etl/extract/test_extract_item_list.py
+++ tests/etl/extract/test_extract_item_list.py
@@ -7,8 +7,13 @@ from mndot_bid_api.etl.extract import item_list
 
 def test_read_item_list_csv(item_list_csv_content):
     item_list_data = item_list.read_item_list_csv(item_list_csv_content)
+    df = item_list_data.raw_items.copy()
 
-    assert item_list_data.raw_items.shape == (7713, 6)
-    df_schemas.RawItems.validate(item_list_data.raw_items)
+    assert df.shape == (7713, 6)
+    df_schemas.RawItems.validate(df)
 
     assert item_list_data.spec_year == "2016"
+
+    invalid_df = df.drop(columns="Spec Year")
+    with pytest.raises(pa.errors.SchemaError):
+        df_schemas.RawItems.validate(invalid_df)
