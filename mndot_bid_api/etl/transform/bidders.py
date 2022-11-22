7a9bf142e2b69e497cc86d5d2fd00cdc99ca937e
depowered <dpwr16@gmail.com>
2022-11-22 00:24:11 +0000

    implement transform bidders and tests

--- mndot_bid_api/etl/transform/bidders.py
+++ mndot_bid_api/etl/transform/bidders.py
@@ -6,4 +6,9 @@ from mndot_bid_api.etl.types import RawBiddersDF, TransformedBiddersDF
 
 @pa.check_types
 def transform_bidders(raw_bidders: RawBiddersDF) -> TransformedBiddersDF:
-    ...
+    df = pd.DataFrame()
+
+    df["id"] = raw_bidders["Bidder Number"].astype(int)
+    df["name"] = raw_bidders["Bidder Name"].str.strip()
+
+    return df
