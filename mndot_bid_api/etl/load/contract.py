a955f64e006e61da7f45f8dee5a45927cb58720c
depowered <dpwr16@gmail.com>
2022-11-26 18:34:08 +0000

    implement load contract functionality and tests

--- mndot_bid_api/etl/load/contract.py
+++ mndot_bid_api/etl/load/contract.py
@@ -1,11 +1,40 @@
-import pandas as pd
+from typing import Any
+
 import pandera as pa
 
+from mndot_bid_api import exceptions
 from mndot_bid_api.etl.df_schemas import TransformedContract
 from mndot_bid_api.etl.types import TransformedContractDF
-from mndot_bid_api.schema import LoadResult
+from mndot_bid_api.operations.contracts import create_contract
+from mndot_bid_api.operations.crud_interface import CRUDInterface
+from mndot_bid_api.schema import ContractCreateData, LoadResult
 
 
-@pa.check_input(TransformedContract.to_schema())
-def load_contract(transformed_contract: TransformedContractDF) -> list[LoadResult]:
-    ...
+@pa.check_io(transformed_contract=TransformedContract.to_schema())
+def load_contract(
+    transformed_contract: TransformedContractDF, contract_interface: CRUDInterface
+) -> list[LoadResult]:
+
+    entries: list[dict[str, Any]] = [
+        row._asdict()
+        for row in transformed_contract.itertuples(index=False, name="Contract")
+    ]
+
+    load_results = []
+
+    for entry in entries:
+        create_data = ContractCreateData(**entry)
+        load_result = LoadResult(
+            model="Contract", operation="create", input_data=create_data
+        )
+        try:
+            create_result = create_contract(create_data, contract_interface)
+            load_result.status_code = 201
+            load_result.record_data = create_result
+        except exceptions.HTTPException as exc:
+            load_result.status_code = exc.status_code
+            load_result.message = exc.detail
+
+        load_results.append(load_result)
+
+    return load_results
---
+++ tests/etl/load/test_load_contract.py
@@ -0,0 +1,67 @@
+from datetime import date
+
+from sqlalchemy.orm import sessionmaker
+
+from mndot_bid_api import enums, schema
+from mndot_bid_api.db import models
+from mndot_bid_api.db.interface import DBModelInterface
+from mndot_bid_api.etl.extract.abstract import read_abstract_csv
+from mndot_bid_api.etl.load.contract import load_contract
+from mndot_bid_api.etl.transform.contract import transform_contract
+
+
+def test_load_contract(
+    abstract_csv_content: str, configured_sessionmaker: sessionmaker
+):
+    # Setup CRUD Interface
+    model = models.Contract
+    interface = DBModelInterface(model, configured_sessionmaker)
+
+    # Extract and transform data
+    abstract_data = read_abstract_csv(abstract_csv_content)
+    transformed_df = transform_contract(
+        abstract_data.raw_contract, abstract_data.winning_bidder_id
+    )
+
+    # Expected result when record already exists
+    input_data = schema.ContractCreateData(
+        id=220005,
+        letting_date=date(year=2022, month=1, day=28),
+        sp_number="5625-20",
+        district=enums.District.DETROIT_LAKES,
+        county=enums.County.OTTER_TAIL,
+        description="LOCATED ON T.H. 108 FROM 420' WEST OF EB T.H. 94 RAMP TO 9TH ST NW.",
+        winning_bidder_id=207897,
+    )
+    already_exists_load_result = schema.LoadResult(
+        model="Contract",
+        operation="create",
+        status_code=303,
+        input_data=input_data,
+        message="Contract already exists at ID 220005",
+    )
+
+    # Load the data
+    load_results = load_contract(transformed_df, interface)
+
+    assert len(load_results) == 1
+    first_load_result = load_results[0]
+    assert first_load_result == already_exists_load_result
+
+    # Expected load result when record doesn't already exist
+    record_data = schema.Contract(data=schema.ContractResult(**input_data.dict()))
+    new_record_load_result = schema.LoadResult(
+        model="Contract",
+        operation="create",
+        status_code=201,
+        input_data=input_data,
+        record_data=record_data,
+    )
+
+    # Delete the record and reload the data
+    interface.delete(220005)
+    load_results = load_contract(transformed_df, interface)
+
+    assert len(load_results) == 1
+    first_load_result = load_results[0]
+    assert first_load_result == new_record_load_result
