02f9df277456b9e6240159ab22fb261c923703c0
depowered <dpwr16@gmail.com>
2022-11-27 19:37:14 +0000

    implement and test load functionality

--- mndot_bid_api/etl/load/items.py
+++ mndot_bid_api/etl/load/items.py
@@ -1,11 +1,48 @@
-import pandas as pd
+from typing import Any
+
 import pandera as pa
 
+from mndot_bid_api import exceptions
 from mndot_bid_api.etl.df_schemas import TransformedItems
 from mndot_bid_api.etl.types import TransformedItemsDF
-from mndot_bid_api.schema import LoadResult
+from mndot_bid_api.operations.crud_interface import CRUDInterface
+from mndot_bid_api.operations.items import create_item, update_item
+from mndot_bid_api.schema import ItemCreateData, ItemUpdateData, LoadResult
 
 
 @pa.check_input(TransformedItems.to_schema())
-def load_items(transformed_items: TransformedItemsDF) -> list[LoadResult]:
-    ...
+def load_items(
+    transformed_items: TransformedItemsDF, item_interface: CRUDInterface
+) -> list[LoadResult]:
+
+    entries: list[dict[str, Any]] = [
+        row._asdict() for row in transformed_items.itertuples(index=False, name="Item")
+    ]
+
+    load_results = []
+
+    for entry in entries:
+        create_data = ItemCreateData(**entry)
+        load_result = LoadResult(
+            model="Item", operation="create", input_data=create_data
+        )
+        try:
+            create_result = create_item(create_data, item_interface)
+            load_result.status_code = 201
+            load_result.record_data = create_result
+        except exceptions.HTTPException as exc:
+            load_result.operation = "update"
+            load_result.message = f"{exc.detail} Redirect to update item."
+            try:
+                item_id = int(exc.detail.split(" ")[-1])
+                update_data = ItemUpdateData(**create_data.dict())
+                update_result = update_item(item_id, update_data, item_interface)
+                load_result.status_code = 200
+                load_result.record_data = update_result
+            except exceptions.HTTPException as err:
+                load_result.status_code = err.status_code
+                load_result.message += f" {err.detail}"
+
+        load_results.append(load_result)
+
+    return load_results
