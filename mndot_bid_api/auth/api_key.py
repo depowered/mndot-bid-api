7f9bf389c23addb8849e76b15ba97765ed62046d
depowered <dpwr16@gmail.com>
2022-11-29 15:55:49 +0000

    create authenticate_api_key

---
+++ mndot_bid_api/auth/api_key.py
@@ -0,0 +1,19 @@
+"""
+Adapted from: https://nilsdebruin.medium.com/fastapi-authentication-revisited-enabling-api-key-authentication-122dc5975680
+"""
+from fastapi import Security
+from fastapi.security.api_key import APIKeyHeader
+
+from mndot_bid_api import exceptions
+
+API_KEY = "mykey123"
+API_KEY_NAME = "access_token"
+
+api_key_header = APIKeyHeader(name=API_KEY_NAME)
+
+
+def authorize_api_key(api_key_header: str = Security(api_key_header)) -> str:
+    if api_key_header and api_key_header == API_KEY:
+        return api_key_header
+    else:
+        exceptions.raise_http_403_forbidden()
