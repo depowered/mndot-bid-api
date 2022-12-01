c0e444404c670f77f20bc1f497ca15cafa310a79
depowered <dpwr16@gmail.com>
2022-12-01 02:04:31 +0000

    refactored env variables and constants

--- main.py
+++ main.py
@@ -8,16 +8,19 @@ from mndot_bid_api.db import database
 from mndot_bid_api.db.load_sample_records import load_sample_records
 
 MODE = os.getenv("MODE")
-DEVELOPMENT_DATABASE_URL = os.getenv("DEVELOPMENT_DATABASE_URL")
-PRODUCTION_DATABASE_URL = os.getenv("PRODUCTION_DATABASE_URL")
+ENGINE_DIALECT = "sqlite:///"
+DEVELOPMENT_DATABASE_PATH = str(os.getenv("DEVELOPMENT_DATABASE_PATH"))
+PRODUCTION_DATABASE_PATH = str(os.getenv("DEVELOPMENT_DATABASE_PATH"))
 
+development_database_url = ENGINE_DIALECT + DEVELOPMENT_DATABASE_PATH
+production_database_url = ENGINE_DIALECT + PRODUCTION_DATABASE_PATH
 
 app = FastAPI(swagger_ui_parameters={"defaultModelsExpandDepth": -1})
 
 
 @app.on_event("startup")
 def startup_event():
-    dev_db_exists = Path("./data/dev-api.db").exists()
+    dev_db_exists = Path(DEVELOPMENT_DATABASE_PATH).exists()
 
     if not MODE or MODE not in ["development", "production"]:
         raise ValueError(
@@ -25,11 +28,11 @@ def startup_event():
         )
 
     if MODE == "development":
-        database.init_sqlite_db(url=DEVELOPMENT_DATABASE_URL)
+        database.init_sqlite_db(url=development_database_url)
         if not dev_db_exists:
             load_sample_records()
 
-    database.init_sqlite_db(url=PRODUCTION_DATABASE_URL)
+    database.init_sqlite_db(url=production_database_url)
 
 
 @app.get("/", include_in_schema=False)
