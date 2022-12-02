43167a13c99c1507b2eed6b9c9d7325fed61ed37
depowered <dpwr16@gmail.com>
2022-12-02 16:43:49 -0600

    configure docker-compose

--- Dockerfile
+++ Dockerfile
@@ -5,13 +5,15 @@ WORKDIR /app
 RUN pip3 install poetry
 RUN poetry config virtualenvs.create false
 
-COPY ./pyproject.toml ./poetry.lock /app
+COPY ./pyproject.toml ./poetry.lock /app/
 RUN poetry install --no-root
 
 RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \
     && apt-get -y install --no-install-recommends sqlite3
 
 COPY . /app
+
+VOLUME "/data"
 
 EXPOSE 8080/tcp
 
--- main.py
+++ main.py
@@ -1,38 +1,20 @@
 import os
-from pathlib import Path
 
 from fastapi import FastAPI
 
 from mndot_bid_api import routers
 from mndot_bid_api.db import database
-from mndot_bid_api.db.load_sample_records import load_sample_records
 
-MODE = os.getenv("MODE")
-ENGINE_DIALECT = "sqlite:///"
-DEVELOPMENT_DATABASE_PATH = str(os.getenv("DEVELOPMENT_DATABASE_PATH"))
-PRODUCTION_DATABASE_PATH = str(os.getenv("PRODUCTION_DATABASE_PATH"))
+DB_FILE = str(os.getenv("DB_FILE"))
+DB_URL = f"sqlite:///data/{DB_FILE}"
 
-development_database_url = ENGINE_DIALECT + DEVELOPMENT_DATABASE_PATH
-production_database_url = ENGINE_DIALECT + PRODUCTION_DATABASE_PATH
 
 app = FastAPI(swagger_ui_parameters={"defaultModelsExpandDepth": -1})
 
 
 @app.on_event("startup")
 def startup_event():
-    dev_db_exists = Path(DEVELOPMENT_DATABASE_PATH).exists()
-
-    if not MODE or MODE not in ["development", "production"]:
-        raise ValueError(
-            "Environment variable MODE is invalid. Valid values: development, production"
-        )
-
-    if MODE == "development":
-        database.init_sqlite_db(url=development_database_url)
-        if not dev_db_exists:
-            load_sample_records()
-    else:
-        database.init_sqlite_db(url=production_database_url)
+    database.init_sqlite_db(url=DB_URL)
 
 
 @app.get("/", include_in_schema=False)
