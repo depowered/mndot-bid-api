edca238d2e7a7f79062ddb074393216c194127bd
depowered <dpwr16@gmail.com>
2022-12-10 00:00:35 +0000

    Add description to OpenAPI docs

--- main.py
+++ main.py
@@ -8,8 +8,40 @@ from mndot_bid_api.db import database
 DB_FILE = str(os.getenv("DB_FILE"))
 DB_URL = f"sqlite:///data/{DB_FILE}"
 
+description = """
+### Purpose
+Backend service for extracting bid information from MnDOT's published 
+[abstracts](https://www.dot.state.mn.us/bidlet/abstract.html) and providing 
+create, read, update and delete operations to manage the extracted data.
 
-app = FastAPI(swagger_ui_parameters={"defaultModelsExpandDepth": -1})
+### Route Categories
+**contract**: General project data for a particular abstract including letting date, 
+SP Number, MnDOT District, etc.
+
+**bidder**: ID and name of prime contractors that have submitted a bid proposal that 
+was ranked as the three lowest bid proposals.
+
+**bid**: Invididual bid records including engineers estimates, winning bidder, and losing bidders.
+
+**invalid_bid**: Bid records for items that do not conform to the MnDOT Standard Specifications.
+
+**item**: Items defined in the MnDOT Standard Specifications.
+
+**etl**: Extract, Transform, Load operations that modify the database records based on CSV uploads 
+for MnDOT Standard Specification item lists or published abstracts.
+
+### Contract
+Created and maintained by: [devin@powergeospatial.xyz](mailto:devin@powergeospatial.xyz)
+
+"""
+
+
+app = FastAPI(
+    title="MnDOT Bid API",
+    description=description,
+    version="0.2.0",
+    swagger_ui_parameters={"defaultModelsExpandDepth": -1},
+)
 
 
 @app.on_event("startup")
